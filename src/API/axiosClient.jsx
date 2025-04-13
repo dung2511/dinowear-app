80ca692b342c4f7274fd5d1282db73fa3e4b1410
dung0811 <103187128+dung2511@users.noreply.github.com>
2025-04-13 11:08:02 +0700

    first commit

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080" || "https://" + process.env.URL_API,
   headers: {
     "content-type": "application/json",
   },
